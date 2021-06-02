abb1a5ec3e0e51c583dc623c27d5b92ab66ae482
Devesh <devmishra88@gmail.com>
2021-06-02 23:07:14 +0530

    desktop category fixed

--- src/components/HomeCategory.js
+++ src/components/HomeCategory.js
@@ -13,15 +13,15 @@ const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 1
+    items: 8
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 1
+    items: 6
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 1
+    items: 4
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },

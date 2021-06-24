b41389e060c41baa26c2d1bb370af7b96c8150a1
Devesh <devmishra88@gmail.com>
2021-06-24 21:38:20 +0530

    Logo added

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,26 +1,18 @@
 import React, {useState, useContext} from 'react';
+import logo from '../assets/images/logo.jpeg';
+
 import {
   useHistory,
   useLocation
 } from "react-router-dom";
+
 import {authContext} from '../context/Auth';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+import {CssBaseline, TextField, Box, Typography, Container, Checkbox, Snackbar, IconButton, FormControlLabel} from '@material-ui/core';
 import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-
-import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
 function Copyright() {
@@ -47,6 +39,13 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(4),
     /*backgroundColor: theme.palette.secondary.main,*/
     backgroundColor: '#00B970',
+  },
+  logowrapper: {
+    margin: theme.spacing(4),
+    /*backgroundColor: theme.palette.secondary.main,*/
+  },
+  logo: {
+    width: '125px',
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -127,9 +126,9 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
+        <Box className={classes.logowrapper}>
+          <img src={logo} className={classes.logo}/>
+        </Box>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
